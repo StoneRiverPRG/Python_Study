4ae33b0ce4d014e1aed0c5d55b5c8dc280e328b0
Stone <6197619+StoneRiverPRG@users.noreply.github.com>
2021-07-18 19:04:31 +0900

    test

---
+++ 24 ascii.py
@@ -0,0 +1,22 @@
+s = 'A'
+
+ord_s = ord(s)
+print(ord_s)  # 65
+
+chr_s = chr(ord_s)
+print(chr_s)  # A
+
+print(s, ord_s, bin(ord_s))
+# TIPS 0b無しで表示
+print(bin(ord_s)[2:])
+
+lower_s = s.lower() # a
+print(ord(lower_s)) # 97
+
+# asciiコード + 32 で小文字へ変換
+# A → a
+print(chr(ord_s + 32)) # a
+
+string = "Hello!"
+for ch in string:
+    print(ch + " = " + str(ord(ch)) + ", bin = " + str(bin(ord(ch))))
